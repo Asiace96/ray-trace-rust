5e7a2166263085252d9488e5c609a4e727cbc6a2
asafb <asafb96@gmail.com>
2024-12-27 19:58:25 +0200

    adding gamma correction (gamma 2) for accurate color intensity

--- src/utility/colors.rs
+++ src/utility/colors.rs
@@ -5,10 +5,22 @@ use crate::utility::interval::Interval;
 
 pub type Color = Vec3;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return f64::sqrt(linear_component);
+    }
+    return 0.0;
+}
+
 pub fn write_color(output: &mut File, pixel_color: Color) {
-    let r = pixel_color.x;
-    let g = pixel_color.y;
-    let b = pixel_color.z;
+    let mut r = pixel_color.x;
+    let mut g = pixel_color.y;
+    let mut b = pixel_color.z;
+
+    //Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255]
     let intensity = Interval::new(0.000, 0.999);
