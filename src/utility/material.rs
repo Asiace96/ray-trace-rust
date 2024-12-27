1830fda8c24f30c5405d143e60ab8083ba0c3c9b
asafb <asafb96@gmail.com>
2024-12-27 22:09:33 +0200

    fuzzy reflection

--- src/utility/material.rs
+++ src/utility/material.rs
@@ -4,7 +4,6 @@ use crate::utility::ray::Ray;
 use crate::utility::vec3;
 
 
-
 pub trait Material {
     fn scatter(
         &self,
@@ -52,11 +51,13 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(c: Color) -> Self {
-        Metal { albedo: c }
+    pub fn new(c: Color, f: f64) -> Self {
+        Metal { albedo: c, 
+                fuzz: if f < 1.0 {f} else {1.0}, }
     }
 }
 
@@ -70,9 +71,10 @@ impl Material for Metal {
             scattered: &mut Ray
             ) -> bool {
 
-            let reflected = vec3::reflect(r_in.direction(), rec.normal);
+            let mut reflected = vec3::reflect(r_in.direction(), rec.normal);
+            reflected = vec3::unit_vector(reflected) + self.fuzz * vec3::random_unit_vector();
             *scattered = Ray::new(rec.p, reflected);
             *attenuation = self.albedo;
-            return true;
+            return vec3::dot(scattered.direction(), rec.normal) > 0.0;
     }
 }
