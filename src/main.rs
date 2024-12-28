260c2a2f97df908340eb656ef81a750d26846fd6
asafb <asafb96@gmail.com>
2024-12-28 22:02:42 +0200

    a final render

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use utility::hittable_list::HittableList;
 use utility::vec3::{self,Point3,Vec3};
 use utility::ray::Ray;
 use utility::interval::Interval;
-use utility::sphere::Sphere;
+use utility::sphere::{self, Sphere};
 use utility::common;
 use utility::camera::Camera;
 use utility::material::{Dielectric, Lambertian, Metal};
@@ -24,7 +24,7 @@ fn main() -> std::io::Result<()> {
     // Constants
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 1200;
-    const SAMPLES_PER_PIXEL: i32 = 100;
+    const SAMPLES_PER_PIXEL: i32 = 500;
     const MAX_DEPTH: i32 = 50;
     const VERTICAL_FIELD_OF_VIEW: f64 = 90.0;
     let LOOK_FROM = Point3::new(0.0,0.0,0.0);
@@ -36,41 +36,58 @@ fn main() -> std::io::Result<()> {
 
     //World
     let mut world = HittableList::new();
-    let material_ground = Rc::new(Lambertian::new(Color::new(0.8,0.8,0.0)));
-    let material_center = Rc::new(Lambertian::new(Color::new(0.1,0.2,0.5)));
-    let material_left   = Rc::new(Dielectric::new(1.50));
-    let material_bubble = Rc::new(Dielectric::new(1.00 / 1.50));
-    let material_right  = Rc::new(Metal::new(Color::new(0.8,0.6,0.2), 0.0));
-
-
+    let groud_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
     world.add(Box::new(Sphere::new(
-                       Point3::new(0.0, -100.5, -1.0),
-                       100.0,
-                       material_ground,
+                       Point3::new(0.0, -1000.0, 0.0),
+                       1000.0,
+                       groud_material,
     )));
 
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = common::random_double();
+            let center = Point3::new(a as f64 + 0.9*common::random_double(), 0.2, b as f64 + 0.9*common::random_double());
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse 
+                    let albedo = Color::random() * Color::random();
+                    let sphere_material = Rc::new(Lambertian::new(albedo));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = common::random_double_range(0.0, 0.5);
+                    let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    // glass
+                    let sphere_material = Rc::new(Dielectric::new(1.5));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Rc::new(Dielectric::new(1.5));
     world.add(Box::new(Sphere::new(
-                       Point3::new(0.0, 0.0, -1.2),
-                       0.5,
-                       material_center,
-    )));
-    
-    world.add(Box::new(Sphere::new(
-                       Point3::new(-1.0, 0.0, -1.0),
-                       0.5,
-                       material_left,
+                       Point3::new(0.0, 1.0, 0.0),
+                       1.0,
+                       material1,
     )));
 
+    let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
     world.add(Box::new(Sphere::new(
-                       Point3::new(-1.0, 0.0, -1.0),
-                       0.4,
-                       material_bubble,
+                       Point3::new(-4.0, 1.0, 0.0),
+                       1.0,
+                       material2,
     )));
 
+    let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
     world.add(Box::new(Sphere::new(
-                       Point3::new(1.0, 0.0, -1.0),
-                       0.5,
-                       material_right,
+                       Point3::new(4.0, 1.0, 0.0),
+                       1.0,
+                       material3,
     )));
 
     //Camera
@@ -81,13 +98,12 @@ fn main() -> std::io::Result<()> {
     camera.max_depth = MAX_DEPTH;
 
     camera.vfov = 20.0; // VERTICAL_FIELD_OF_VIEW
-    camera.look_from = Point3::new(-2.0, 2.0, 1.0); // LOOK_FROM;
-    camera.look_at = Point3::new(0.0, 0.0, -1.0); // LOOK_AT;
+    camera.look_from = Point3::new(13.0, 2.0, 3.0); // LOOK_FROM;
+    camera.look_at = Point3::new(0.0, 0.0, 0.0); // LOOK_AT;
     camera.vup = Vec3::new(0.0, 1.0, 0.0); // VUP;
 
-    //using a large aperture 
-    camera.defocus_angle = 10.0; // DEFOCUS_ANGLE
-    camera.focus_dist = 3.4; // FOCUS_DIST
+    camera.defocus_angle = 0.6; // DEFOCUS_ANGLE
+    camera.focus_dist = FOCUS_DIST;
     camera.initialize();
 
     // Make file
